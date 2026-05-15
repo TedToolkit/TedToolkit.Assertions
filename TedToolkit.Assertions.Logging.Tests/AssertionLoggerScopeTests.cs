a6f51b3c20aaf007576623bc673074a2495c61cd
秋水 <1123993881@qq.com>
2026-05-15 10:27:43 +0800

    fix: header name fix.

--- TedToolkit.Assertions.Logging.Tests/AssertionLoggerScopeTests.cs
+++ TedToolkit.Assertions.Logging.Tests/AssertionLoggerScopeTests.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="LoggerScopeTests.cs" company="TedToolkit">
+// <copyright file="AssertionLoggerScopeTests.cs" company="TedToolkit">
 // Copyright (c) TedToolkit. All rights reserved.
 // Licensed under the LGPL-3.0 license. See COPYING, COPYING.LESSER file in the project root for full license information.
 // </copyright>
